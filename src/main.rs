ad773d6da41170ed06de245b1530b68e79a7de41
Shane Celis <shane.celis@gmail.com>
2024-05-02 16:54:04 -0400

    style: Run cargo fmt.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::env;
 use english_numbers::*;
+use std::env;
 
 fn main() {
-for argument in env::args() {
-    println!("{argument}");
-}
-    let count: i64 = env::args().skip(1).next().map(|s| s.parse().expect("Max number")).unwrap_or(1_000_000);
+    let count: i64 = env::args()
+        .skip(1)
+        .next()
+        .map(|s| s.parse().expect("Max number"))
+        .unwrap_or(1_000_000);
     print!("{}", convert_all_fmt(0));
     for i in 1..=count {
         print!(", {}", convert_all_fmt(i));
