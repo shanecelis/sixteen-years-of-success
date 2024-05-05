80e203080fca48120e12141ee2fd2dbb64528210
Shane Celis <shane.celis@gmail.com>
2024-05-05 16:40:58 -0400

    style: Run cargo fmt.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use numbers::*;
-use english_numbers;
-use count_write::CountWrite;
-use std::io::{self, Write};
-use std::env;
 use clap::{self, arg, command, value_parser, ArgAction};
+use count_write::CountWrite;
+use english_numbers;
 use human_repr::HumanCount;
+use numbers::*;
+use std::env;
+use std::io::{self, Write};
 
 #[derive(clap::ValueEnum, Debug, Clone)]
 #[clap(rename_all = "kebab_case")]
@@ -30,19 +30,23 @@ const DEFAULT_MAX: i64 = 1_000_000;
 fn main() -> std::io::Result<()> {
     let matches = command!() // requires `cargo` feature
         .disable_version_flag(true)
-        .arg(arg!(-c --count "Show the count in bytes")
-            .action(ArgAction::SetTrue))
-        .arg(arg!(-H --humanize "Human readable byte count")
-            .action(ArgAction::SetTrue))
-        .arg(arg!(-l --language [lang] "Choose language")
-             .action(ArgAction::Append)
-             .value_parser(clap::builder::EnumValueParser::<LanguageArg>::new()))
-        .arg(arg!(-u --upto [upto] "Only count up to so many bytes")
-             .num_args(1)
-             .value_parser(value_parser!(u64)))
-        .arg(arg!([range] "Provide range [min max] or [max]")
-             .num_args(0..=2)
-             .value_parser(value_parser!(i64)))
+        .arg(arg!(-c --count "Show the count in bytes").action(ArgAction::SetTrue))
+        .arg(arg!(-H --humanize "Human readable byte count").action(ArgAction::SetTrue))
+        .arg(
+            arg!(-l --language [lang] "Choose language")
+                .action(ArgAction::Append)
+                .value_parser(clap::builder::EnumValueParser::<LanguageArg>::new()),
+        )
+        .arg(
+            arg!(-u --upto [upto] "Only count up to so many bytes")
+                .num_args(1)
+                .value_parser(value_parser!(u64)),
+        )
+        .arg(
+            arg!([range] "Provide range [min max] or [max]")
+                .num_args(0..=2)
+                .value_parser(value_parser!(i64)),
+        )
         .get_matches();
     let (min, max) = match matches.get_many::<i64>("range") {
         None => (DEFAULT_MIN, DEFAULT_MAX),
@@ -50,7 +54,7 @@ fn main() -> std::io::Result<()> {
             let first = *values.next().unwrap();
             match values.next() {
                 None => (DEFAULT_MIN, first),
-                Some(v) => (first, *v)
+                Some(v) => (first, *v),
             }
         }
     };
@@ -58,10 +62,9 @@ fn main() -> std::io::Result<()> {
     let upto: Option<u64> = matches.get_one("upto").cloned();
     let langs: Vec<Language> = matches
         .get_many("language")
-        .map(|x: clap::parser::ValuesRef<LanguageArg>|
-             x
-             .map(|y: &LanguageArg| Language::from(y.clone()))
-             .collect())
+        .map(|x: clap::parser::ValuesRef<LanguageArg>| {
+            x.map(|y: &LanguageArg| Language::from(y.clone())).collect()
+        })
         .unwrap_or(vec![Language::English]);
 
     if *matches.get_one("count").unwrap() {
@@ -71,7 +74,9 @@ fn main() -> std::io::Result<()> {
                 type_numbers(&mut byte_counter, &langs, min, |_, i| i < max)?;
             }
             Some(max_bytes) => {
-                let number = type_numbers(&mut byte_counter, &langs, min, |w, _| w.count() <= max_bytes)?;
+                let number = type_numbers(&mut byte_counter, &langs, min, |w, _| {
+                    w.count() <= max_bytes
+                })?;
                 println!("Reached number {number} in {} bytes.", byte_counter.count());
                 return Ok(());
             }
@@ -92,7 +97,7 @@ fn convert(lang: Language, number: i64) -> String {
         let format = english_numbers::Formatting {
             title_case: false,
             // conjunctions: false,
-            .. english_numbers::Formatting::all()
+            ..english_numbers::Formatting::all()
         };
         english_numbers::convert(number, format)
     } else {
@@ -100,10 +105,15 @@ fn convert(lang: Language, number: i64) -> String {
     }
 }
 
-fn type_numbers<W: Write, F: FnMut(&W, i64) -> bool>(output: &mut W, langs: &[Language], min: i64, mut cond: F) -> io::Result<i64> {
+fn type_numbers<W: Write, F: FnMut(&W, i64) -> bool>(
+    output: &mut W,
+    langs: &[Language],
+    min: i64,
+    mut cond: F,
+) -> io::Result<i64> {
     let mut i = min;
     loop {
-        if ! cond(&output, i) {
+        if !cond(&output, i) {
             break;
         };
         for lang in langs {
